408ecc60d1a9403bfec511bb8557ad7a4fd85e07
erycpc <ericmbithiofficial@gmail.com>
2026-09-04 14:59:06 +0300

    Created Signup Endpoint and Fixed Signup user creation hook

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,53 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
 export async function signup(req, res) {
-  res.send("Signup route");
+  const { fullname, email, password } = req.body;
+
+  try {
+    if (!fullname || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters long" });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use, please use a different email" });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1; // Random number between 1 and 100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`
+
+    const newUser = await User.create({
+      fullname,
+      email,
+      password,
+      avatar: randomAvatar
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, 
+      { 
+        expiresIn: "1h" 
+      });
+
+      res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true, // prevent xss attacks
+        sameSite: "strict", // prevent csrf attacks
+        secure: process.env.NODE_ENV === "production", // only send cookie over https in production
+      }); 
+
+      res.status(201).json({ message: "User created successfully", token });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export async function login(req, res) {
---
+++ backend/src/models/User.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+        default: '',
+    },
+    profilePicture: {
+        type: String,
+        default: '',
+    },
+    nativeLanguage: {
+        type: String,
+        default: '',
+    },
+    learningLanguage: {
+        type: [String],
+        default: [],
+    },
+    isOnboarded: {
+        type: Boolean,
+        default: false,
+    },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+}, { timestamps: true }); // This option automatically adds createdAt and updatedAt fields to the schema
+
+// Pre-save hook to hash the password before saving the user document
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) {
+        return;
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
