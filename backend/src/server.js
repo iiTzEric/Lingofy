468b20a546d8734f53a67769d3c171e0b3f94eee
erycpc <ericmbithiofficial@gmail.com>
2026-09-04 13:31:11 +0300

    Setting Up Project

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/signup', (req, res) => {
+  res.send('Signup endpoint');
+});
+
+app.get('/api/login', (req, res) => {
+  res.send('Login endpoint');
+});
+
+app.get('/api/logout', (req, res) => {
+  res.send('Logout endpoint');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
