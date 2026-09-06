e8d49d9f63a043a457fa471dc25362993f5b4d27
erycpc <ericmbithiofficial@gmail.com>
2026-09-07 00:05:41 +0300

    Added getFriendRequests and outgoingFriendReqs Endpoint

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getRecommendedUsers, getMyFriends, sendFriendRequest, acceptFriendRequest } from '../controllers/user.controller.js';
+import { 
+    getRecommendedUsers, 
+    getMyFriends, 
+    sendFriendRequest, 
+    acceptFriendRequest, 
+    getFriendRequests,
+    getOutgoingFriendReqs 
+} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
@@ -12,5 +19,8 @@ router.get('/friends', getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
 router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests", getFriendRequests);
+router.get("outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router;
