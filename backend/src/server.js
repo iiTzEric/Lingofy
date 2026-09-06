f4cbb4d90362666677cfe54411c2a4e408da780b
erycpc <ericmbithiofficial@gmail.com>
2026-09-07 00:18:52 +0300

    Added Chat Endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
+import chatRoutes from './routes/chat.route.js';
 
 import { connectDB } from './lib/db.js';
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
