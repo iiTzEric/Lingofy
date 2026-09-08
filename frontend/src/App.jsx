0c8e55db70dc23ff2bf387e5154364e5547c9a2d
erycpc <ericmbithiofficial@gmail.com>
2026-09-08 14:08:23 +0300

    Tanstack Query Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,23 @@ import ChatPage from "./pages/ChatPage.jsx"
 import OnboardingPage from "./pages/OnboardingPage.jsx"
 
 import { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import { axiosInstance } from "./lib/axios.js"
 
 const App = () => {
+  // tanstack query
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+
+  console.log(data);
+
+
   return (
     <div className="h-screen"  data-theme="coffee">
       <Routes>
