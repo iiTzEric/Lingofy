c78f21ee0a2748bacb7458990bf1bbed7ae26492
erycpc <ericmbithiofficial@gmail.com>
2026-09-08 16:21:43 +0300

    Implemented Sign Up Page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,11 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 
+import axios from 'axios';
+
+// Ensure all axios requests include cookies/credentials
+axios.defaults.withCredentials = true;
+
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
